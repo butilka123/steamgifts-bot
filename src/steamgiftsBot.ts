761b251cbdd7a012c709b5941b3378d35fe736ed
butilka123 <private>
2022-06-04 13:31:43 +0200

    feat: bot working

--- src/steamgiftsBot.ts
+++ src/steamgiftsBot.ts
@@ -1,40 +1,126 @@
 import { AxiosRequestConfig, AxiosResponse } from "axios";
+import cheerio from "cheerio";
 import * as fs from "fs";
 import { Website } from "../src/Website";
 
 export class SteamgiftsBot {
   website = new Website();
 
-  html: string | undefined = undefined;
+  currentPage: number = 1;
 
-  page: number = 1;
+  xsrf_token: string = "";
+
+  headers = {
+    Cookie: "PHPSESSID=" + fs.readFileSync("./src/cookie.txt", "utf-8"),
+  };
+
+  html: string = "";
 
   constructor() {
-    // this.getPage()
-
-    // Temporarily for testing to not spam requests - this.getPage() response in a file
-    this.html = fs.readFileSync("C://Repositories/file.txt", "utf-8");
+    this.getGames();
   }
 
-  getPage(): void {
+  async getPage(websiteUrl) {
     try {
-      let headers = {
-        Cookie: "PHPSESSID=" + fs.readFileSync("./src/cookie.txt", "utf-8"),
-      };
-
-      let config: AxiosRequestConfig = {
-        url: "https://www.steamgifts.com/",
+      const config: AxiosRequestConfig = {
+        url: websiteUrl,
         method: "get",
-        headers: headers,
+        headers: this.headers,
       };
 
-      this.website.getPage(config).then((response: AxiosResponse) => {
-        this.html = response.data;
+      await this.website.getPage(config).then((response: AxiosResponse) => {
+        if (response.status === 200) {
+          this.html = response.data;
+        } else {
+          throw console.error(response.status + " - " + response.statusText);
+        }
       });
     } catch (error) {
       throw error;
     }
   }
 
-  getGames(): any {}
+  async getGames() {
+    try {
+      await this.getPage(
+        "https://www.steamgifts.com/giveaways/search?page=" + this.currentPage
+      );
+
+      console.log("Processing games from Page " + this.currentPage);
+
+      const $ = cheerio.load(this.html);
+
+      const points: number = Number($(".nav__points").text());
+      this.xsrf_token = $('[name="xsrf_token"]').val();
+
+      const gameList = $(".giveaway__row-inner-wrap");
+
+      for (let game of gameList) {
+        const gameCost: number = Number(
+          $(game)
+            .find(".giveaway__heading__thin")
+            .last()
+            .text()
+            .replace(/[^0-9]/g, "")
+        );
+
+        const gameName: string = $(game)
+          .find(".giveaway__heading__name")
+          .text();
+
+        const gameCode: string = $(game)
+          .find(".giveaway__heading__name")
+          .attr("href")
+          .split("/")[2];
+
+        if (points - gameCost < 0) {
+          console.log(
+            "Not enough Points to enter the next giveaway. Waiting 1 hour to get more Points"
+          );
+          await new Promise((resolve) => setTimeout(resolve, 1000 * 60 * 60)); // 1 h
+          this.getGames();
+          break;
+        }
+        await new Promise((resolve) => setTimeout(resolve, 1000 * 2)); // 2 sec
+        await this.enterGiveaway(
+          "https://www.steamgifts.com/ajax.php",
+          gameCode,
+          gameName
+        );
+      }
+
+      console.log("List of games ended. Waiting 2 Minutes to continiue");
+      await new Promise((resolve) => setTimeout(resolve, 1000 * 60 * 2)); // 2 min
+      this.currentPage = this.currentPage + 1;
+      this.getGames();
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async enterGiveaway(url: string, gameCode: string, gameName: string) {
+    try {
+      const payload = `xsrf_token=${this.xsrf_token}&do=entry_insert&code=${gameCode}`;
+
+      const config: AxiosRequestConfig = {
+        url: url,
+        method: "post",
+        headers: this.headers,
+        data: payload,
+      };
+
+      this.website.getPage(config).then((response: AxiosResponse) => {
+        if (response.status === 200) {
+          console.log("> Entered giveaway: " + gameName);
+        } else {
+          throw console.error(response.status + " - " + response.statusText);
+        }
+      });
+    } catch (error) {
+      throw error;
+    }
+  }
 }
+
+// .giveaway__column--contributor-level--negative
+// .is-faded
